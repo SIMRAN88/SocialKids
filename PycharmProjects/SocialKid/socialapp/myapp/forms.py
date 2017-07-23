df60fb2b824afa4b8e8dc4d14321e390b5fdeb73
rajsimran1996@gmail.com <Your Email Id>
2017-07-23 23:02:18 +0530

    Adding form validations

--- PycharmProjects/SocialKid/socialapp/myapp/forms.py
+++ PycharmProjects/SocialKid/socialapp/myapp/forms.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django import forms
 from models import User, PostModel, LikeModel, CommentModel
 
@@ -7,6 +8,19 @@ class SignUpForm(forms.ModelForm):
         model = User
         fields = ['email', 'username', 'name', 'password']
 
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if len(username) < 5:
+            print 'Username short'
+            raise forms.ValidationError('Username must be greater than 4 characters')
+        return username
+
+    def clean_password(self):
+        password = self.cleaned_data['password']
+        if len(password) <= 5:
+            print 'Password short'
+            raise forms.ValidationError('Password must be greater than 5 characters')
+        return password
 
 class LoginForm(forms.ModelForm):
     class Meta:
